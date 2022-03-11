2c0170370883f647bff589ee603c87a65daadece
Ildar <bakirovildar2011@mail.ru>
2022-03-11 17:41:06 +0500

    add new layout 'Header'

---
+++ src/layouts/header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <span className='contacts'>Contacts</span>
+            <span className='auth'>Authorization</span>
+        </div>
+    )
+}
+
+export default Header
