2ecb4766d1082b26bfabace45fca7e55ab303dcd
Ildar <bakirovildar2011@mail.ru>
2022-03-14 13:03:50 +0500

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import Header from "./layouts/header/Header";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Authorization from "./pages/Authorization";
+import Contacts from "./pages/Contacts";
 
 function App() {
-  return (
-    <div className="App">
-     <h1>Hello</h1>
-    </div>
-  );
+
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Header/>
+                <div className='main-class'>
+                    <Routes>
+                        <Route path={'/login'} element={<Authorization/>}/>
+                        <Route path={'/contacts'} element={<Contacts/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    )
 }
 
 export default App;
