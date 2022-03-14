886ab1e062a44f158d9ce21d19120516b093dd48
Ildar <bakirovildar2011@mail.ru>
2022-03-14 12:59:30 +0500

    added authorization check

--- src/layouts/header/Header.js
+++ src/layouts/header/Header.js
@@ -1,11 +1,21 @@
 import React from "react";
 import './Header.css'
+import {Button} from "@mui/material";
+import {Link} from "react-router-dom";
+import {useAuth} from "../../hooks/use-auth";
 
 const Header = () => {
+
+    const {isAuth, email} = useAuth()
+
     return (
         <div className='header'>
-            <span className='contacts'>Contacts</span>
-            <span className='auth'>Authorization</span>
+            <span className='contacts'>
+                    <Button variant="contained" disabled={isAuth ? false : true} color="success" component={Link}  to='/contacts'>
+                        Contacts
+                    </Button>
+            </span>
+            <div className='auth'><Button variant="contained" component={Link} to='/login'>Авторизация</Button></div>
         </div>
     )
 }
